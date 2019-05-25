95aa0bc1a2de75d9ea54f5fb24e162fd411fa4b0
devhackerone <devhackeronebsa@gmail.com>
2019-05-25 18:48:27 +0300

    Add files

---
+++ index.js
@@ -0,0 +1,21 @@
+const API_URL = 'https://api.git--hub.com/repos/sahanr/street-fighter/contents/fighters.json';
+
+const rootElement = document.getElementById('root');
+
+rootElement.innerText = 'Loading...';
+
+
+fetch(API_URL)
+    .then( response => response.json())
+
+    .then(file => {
+        const fighters = JSON.parse(atob(file.content));
+        const names = fighters.map(it => it.name).join('\n');
+
+        rootElement.innerText = names;
+    })
+
+    .catch(error => {
+        console.warn(error);
+        root.innerText = 'Failed to load data';
+    });
